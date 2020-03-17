765f6b6944e6642bf043b7f004f4ae655d8c790d
Naomi <614231+healingbrew@users.noreply.github.com>
2020-03-17 06:24:06 +0000

    Initial commit

---
+++ UltrawidePatcher/PatcherFlags.cs
@@ -0,0 +1,39 @@
+﻿using DragonLib.CLI;
+using JetBrains.Annotations;
+
+namespace UltrawidePatcher
+{
+    [MeansImplicitUse]
+    [PublicAPI]
+    public class PatcherFlags : ICLIFlags
+    {
+        [NotNull]
+        [UsedImplicitly]
+        [CLIFlag("file", Positional = 0, IsRequired = true, Help = "File to mod", Category = "General Options")]
+        public string File { get; set; } = "";
+
+        [CLIFlag("fov-signature", Default = "39 8E E3 3F", Help = "Signature for FOV", Category = "Signature Options")]
+        public string? FOVSignature { get; set; }
+        
+        [CLIFlag("fov-target", Default = "26 B4 17 40", Help = "Target bytes for FOV Adjustment", Category = "Target Options")]
+        public string? FOVTarget { get; set; }
+        
+        [CLIFlag("fov-calculate", Default = "2560x1080", Help = "Calculate FOV from width/height", Category = "Target Options")]
+        public string? FOVCalc { get; set; }
+        
+        [CLIFlag("ue4-pillarbox-signature", Default = "F6 41 2C ??", Help = "Signature for Unreal Engine 4 Pillarboxes", Category = "Signature Options")]
+        public string? UE4PillarboxSignature { get; set; }
+        
+        [CLIFlag("ue4-pillarbox-target", Default = "F6 41 2C 00", Help = "Target bytes for UE4 Pillarbox removal", Category = "Target Options")]
+        public string? UE4PillarboxTarget { get; set; }
+        
+        [CLIFlag("ue4-ratio-signature", Default = "3B 8E E3 3F", Help = "Signature for Unreal Engine 4 Ratio, uses --fov-target/--fov-calculate", Category = "Signature Options")]
+        public string? UE4RatioSignature { get; set; }
+        
+        [CLIFlag("ue4-fov-signature", Default = "35 FA 0E 3C A4", Help = "Signature for Unreal Engine 4 YFOV", Category = "Signature Options")]
+        public string? UE4YFOVSignature { get; set; }
+        
+        [CLIFlag("ue4-fov-target", Default = "35 FA 3E 3C A4", Help = "Target bytes for UE4 YFOV adjustment", Category = "Target Options")]
+        public string? UE4YFOVTarget { get; set; }
+    }
+}
---
+++ UltrawidePatcher/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+using System.Linq;
+using DragonLib;
+using DragonLib.CLI;
+using DragonLib.IO;
+using JetBrains.Annotations;
+
+namespace UltrawidePatcher
+{
+    [PublicAPI]
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger.PrintVersion("WSGF");
+            var flags = CommandLineFlags.ParseFlags<PatcherFlags>(CommandLineFlags.PrintHelp, args);
+            if (flags == null) return;
+
+            
+            var fovTarget = flags.FOVTarget?.ToHexOctetsB();
+            if (!string.IsNullOrWhiteSpace(flags.FOVCalc))
+            {
+                var parts = flags.FOVCalc.Split('x', ':');
+                if (parts.Length >= 2 && float.TryParse(parts[0], out var width) && float.TryParse(parts[1], out var height))
+                {
+                    fovTarget = BitConverter.GetBytes(width / height);
+                }
+            }
+
+            var buffer = File.ReadAllBytes(flags.File);
+
+            ReplaceSignature(buffer, "FOV", flags.FOVSignature, fovTarget);
+            ReplaceSignature(buffer, "Unreal Engine 4 Aspect Ratio", flags.UE4RatioSignature, fovTarget);
+            ReplaceSignature(buffer, "Unreal Engine 4 Pillarbox", flags.UE4PillarboxSignature, flags.UE4PillarboxTarget?.ToHexOctetsB());
+            ReplaceSignature(buffer, "Unreal Engine 4 FOV", flags.UE4YFOVSignature, flags.UE4YFOVTarget?.ToHexOctetsB());
+            
+            File.WriteAllBytes(Path.ChangeExtension(flags.File, ".ultrawide.exe"), buffer.ToArray());
+        }
+
+        private static void ReplaceSignature(Span<byte> buffer, string name, string? signature, Span<byte> target)
+        {
+            if (string.IsNullOrWhiteSpace(signature) || target.Length == 0) return;
+            var cursor = 0;
+            var signatureLength = signature.ToHexOctetsA().Length;
+            if (signatureLength > buffer.Length) return;
+            Logger.Log(ConsoleSwatch.COLOR_RESET, null, false, Console.Out, "WSGF", null, "Searching for " + name + " signature ");
+            Logger.Log(ConsoleSwatch.XTermColor.Fuchsia, true, Console.Out, null, null, signature);
+            while (cursor + signatureLength < buffer.Length && cursor > -1)
+            {
+                Logger.Debug("WSGF", $"Searching at 0x{cursor:X2}");
+                var tmp = buffer.Slice(cursor).FindPointerFromSignature(signature);
+                if (tmp == -1) break;
+                cursor += tmp;
+                Logger.Log(ConsoleSwatch.COLOR_RESET, null, false, Console.Out, "WSGF", null, "Found signature at offset ");
+                Logger.Log(ConsoleSwatch.XTermColor.OrangeRed, false, Console.Out, null, null, $"0x{cursor:X2} ");
+                Logger.Log(ConsoleSwatch.XTermColor.HotPink, true, Console.Out, null, null, $"{buffer.Slice(cursor, signatureLength).ToHexString()}");
+                target.CopyTo(buffer.Slice(cursor));
+                cursor += signatureLength;
+            }
+        }
+    }
+}
